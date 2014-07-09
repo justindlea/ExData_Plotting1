83bc4f19df86d87f3656a129ea2cd4d7093661e0
jlea@ltsnet.net <jlea@ltsnet.net>
2014-07-09 16:18:16 -0400

    Update to plot1.R

--- plot1.R
+++ plot1.R
@@ -2,6 +2,6 @@ setClass("myDate");
 setAs("character", "myDate", function(from) as.Date(from, format="%d/%m/%Y"));
 data<-read.delim("eda\\household_power_consumption.txt", sep=";", header=T, na.strings="?", colClasses=c('myDate', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'));
 data<-subset(data, Date>=as.Date("2007-02-01") & Date<=as.Date("2007-02-02"));
-png("plot1.png");
+png("ExData_Plotting1\\plot1.png");
 hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)");
 dev.off();
