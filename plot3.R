5c4d146a8faaf94ba49afa617cb2c1e8d64b5b80
jlea@ltsnet.net <jlea@ltsnet.net>
2014-07-09 17:20:18 -0400

    Creation of plot3.R and plot3.png

---
+++ plot3.R
@@ -0,0 +1,11 @@
+setClass("myDate");
+setAs("character", "myDate", function(from) as.Date(from, format="%d/%m/%Y"));
+data<-read.delim("eda\\household_power_consumption.txt", sep=";", header=T, na.strings="?", colClasses=c('myDate', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'));
+data<-subset(data, Date>=as.Date("2007-02-01") & Date<=as.Date("2007-02-02"));
+data$datetime <- strptime(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S");
+png("ExData_Plotting1\\plot3.png");
+plot(data$datetime, data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering");
+lines(data$datetime, data$Sub_metering_2, col="red");
+lines(data$datetime, data$Sub_metering_3, col="blue");
+legend("topright", col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1));
+dev.off();
